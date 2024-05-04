1b12cb333c639651240e6158b6885eaf60f2132c
VinnRe <22-03804@g.batstate-u.edu.ph>
2024-05-04 21:36:15 +0800

    Add Moderate Page and styles

---
+++ frontend/src/pages/Modereate.tsx
@@ -0,0 +1,20 @@
+import './styles/Moderate.css'
+
+const Moderate = () => {
+    return (
+        <div className="moderate-page">
+            <h1>TESTING TESTING</h1>
+            <h1>TESTING TESTING</h1>
+            <h1>TESTING TESTING</h1>
+            <h1>TESTING TESTING</h1>
+            <h1>TESTING TESTING</h1>
+            <h1>TESTING TESTING</h1>
+
+            <section className="approval-section">
+                {/* INSERT ALL ITEMS THAT NEEDS APPROVAL */}
+            </section>
+        </div>
+    )
+}
+
+export default Moderate
