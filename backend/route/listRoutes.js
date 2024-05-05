250929535c575c13e26aaf869353bdc562b983a7
oocim <116318345+oocim@users.noreply.github.com>
2024-05-05 10:14:42 +0800

    Add listing moderation logic

--- backend/route/listRoutes.js
+++ backend/route/listRoutes.js
@@ -6,7 +6,9 @@ const authController = require("../controller/authController");
 const router = express.Router();
 
 router.route("/create").post(authController.protect, listController.createList);
-router.route("/fetch/all").get(listController.fetchAll);
+router.route("/approve").post(authController.protect, listController.approveList);
+router.route("/fetch/unapproved").get(listController.fetchUnapproved);
+router.route("/fetch/approved").get(listController.fetchApproved);
 router.route("/fetch/search").post(listController.searchList);
 router.route("/fetch/tags").post(listController.filterByTags);
 
