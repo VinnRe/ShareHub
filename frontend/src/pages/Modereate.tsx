79a9c732955a3b9bf1446d77d0b12de52d140baa
VinnRe <22-03804@g.batstate-u.edu.ph>
2024-05-08 15:25:15 +0800

    Add fetching of unapproved items

--- frontend/src/pages/Modereate.tsx
+++ frontend/src/pages/Modereate.tsx
@@ -1,7 +1,46 @@
 import './styles/Moderate.css'
 import ApprovalItem from '../components/ApprovalItem'
+import { useState, useEffect } from 'react'
+
+interface ListedProps {
+    _id: string;
+    title: string;
+    details: string;
+    media: File;
+    creator: string;
+    createdAt: Date;
+    tags: string[];
+    ownerName: string;
+}
 
 const Moderate = () => {
+    const [listed, setListed] = useState<any>(null)
+
+    const fetchListed = async () => {
+        const response = await fetch("/api/list/fetch/unapproved")
+        const json = await response.json()
+
+        console.log(json)
+    
+        console.log(json)
+        if (response.ok) {
+          const listingData = json.map((item: any) => {
+            console.log(item)
+            return {
+                ...item.listData,
+                ownerName: item.ownerInfo.name
+            }
+          })
+          setListed(listingData)
+          console.log(listingData)
+        }
+    }
+
+    useEffect(() => {
+        fetchListed()
+        return
+    }, [])
+
     return (
         <section className="moderate-page">
             <main className="approval-section">
@@ -11,17 +50,22 @@ const Moderate = () => {
                 </div>
                 <div className="tb-approved">
                     <div className="approval-list">
-                        <ApprovalItem />
-                        <ApprovalItem />
-                        <ApprovalItem />
-                        <ApprovalItem />
-                        <ApprovalItem />
-                        <ApprovalItem />
-                        <ApprovalItem />
-                        <ApprovalItem />
-                        <ApprovalItem />
-                        <ApprovalItem />
-                        <ApprovalItem />
+                        {listed ? (
+                            listed.map((list: ListedProps) => (
+                                <ApprovalItem
+                                    key={list._id}
+                                    itemID={list._id}
+                                    title={list.title}
+                                    creator={list.ownerName}
+                                    createdAt={new Date(list.createdAt)}
+                                    details={list.details}
+                                    media={list.media}
+                                    tags={list.tags}
+                                />
+                            ))
+                        ) : (
+                            <p>Loading resources...</p>
+                        )}
                     </div>
                 </div>
             </main>
