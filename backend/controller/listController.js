46d6e835f9fd0ef964063c1611451961ea2c2913
oocim <116318345+oocim@users.noreply.github.com>
2024-05-08 14:41:37 +0800

    Add delete routes

--- backend/controller/listController.js
+++ backend/controller/listController.js
@@ -69,6 +69,19 @@ exports.approveList = catchAsync(async (req, res) => {
   }
 });
 
+exports.deleteList = catchAsync(async (req, res) => {
+  const { listId } = req.body;
+  try {
+    const list = await List.findByIdAndDelete(listId);
+    if (!list) {
+      return res.status(404).json({ error: "List not found" });
+    }
+    res.status(200).json({ message: "List deleted successfully" });
+  } catch (err) {
+    res.status(500).json({ error: "Couldn't delete list" });
+  }
+});
+
 exports.fetchOptions = catchAsync(async (req, res) => {
     const { options, name } = req.body;
     const requestedUser = await User.findOne({ name: name });
--- backend/route/listRoutes.js
+++ backend/route/listRoutes.js
@@ -7,6 +7,7 @@ const router = express.Router();
 
 router.route("/create").post(authController.protect, listController.createList);
 router.route("/approve").post(authController.protect, listController.approveList);
+router.route("/delete").post(authController.protect, listController.deleteList);
 router.route("/fetch/unapproved").get(listController.fetchUnapproved);
 router.route("/fetch/approved").get(listController.fetchApproved);
 router.route("/fetch/search").post(listController.searchList);
